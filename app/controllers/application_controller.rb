6af5e577aec4d69cd22ead0471476a0cbe2db71f
gdhar92 <gdhar@chicagobooth.edu>
2021-04-19 05:58:21 +0000

    3rd commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_form.html.erb"})
   end
 
+  def calculate_square
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 end
